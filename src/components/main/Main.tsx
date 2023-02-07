8bde2e4341d28a3e89f8bc781718491526b4699e
tanon10 <antoniocaldas3101@gmail.com>
2023-02-06 19:01:15 -0500

    Change: Main and styles global, main

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -7,72 +7,21 @@ const Main = () => {
         <section className="main-container-left">
           <div className="container-top-left">
             <h2>Deporte</h2>
+
             <article className="article-main">
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div className="article-main-bottom">
+              <div className="article-content">
                 <h3>Test Article Left</h3>
-                <hr />
+
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
                 </p>
-                <button>Leer Mas</button>
               </div>
-            </article>
-            <article className="article-main">
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
-              </div>
-            </article>
-            <article className="article-main">
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
-              </div>
-            </article>
-            <article className="article-main">
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
+              <div className="article-image">
+                <img
+                  src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
+                  alt=""
+                />
               </div>
             </article>
           </div>
@@ -92,7 +41,6 @@ const Main = () => {
                   recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
                   accusantium, minima eaque. Quo, delectus expedita.
                 </p>
-                <button>Leer Mas</button>
               </div>
             </article>
             <article>
@@ -109,7 +57,6 @@ const Main = () => {
                   recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
                   accusantium, minima eaque. Quo, delectus expedita.
                 </p>
-                <button>Leer Mas</button>
               </div>
             </article>
             <article>
@@ -126,7 +73,6 @@ const Main = () => {
                   recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
                   accusantium, minima eaque. Quo, delectus expedita.
                 </p>
-                <button>Leer Mas</button>
               </div>
             </article>
             <article>
@@ -143,7 +89,6 @@ const Main = () => {
                   recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
                   accusantium, minima eaque. Quo, delectus expedita.
                 </p>
-                <button>Leer Mas</button>
               </div>
             </article>
           </div>
@@ -153,141 +98,24 @@ const Main = () => {
 
         <section className="main-container-right">
           <div className="container-full-right">
-            <h2>Tecnologia</h2>
-            <article>
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
+            <div className="category">
+              <h2>Ultimo Minuto</h2>
+            </div>
+            <article className="article-main-right">
+              <div className="article-image-right">
+                <img
+                  src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
+                  alt=""
+                />
               </div>
-            </article>
-            <article>
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
+              <div className="article-content-right">
                 <h3>Test Article Left</h3>
-                <br />
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Molestias corporis repellat quaerat incidunt sed ipsum
                   recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
                   accusantium, minima eaque. Quo, delectus expedita.
                 </p>
-                <button>Leer Mas</button>
-              </div>
-            </article>
-            <article>
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
-              </div>
-            </article>
-            <article>
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
-              </div>
-            </article>
-            <article>
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
-              </div>
-            </article>
-            <article>
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
-              </div>
-            </article>
-            <article>
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
-              </div>
-            </article>
-            <article>
-              <img
-                src="https://www.nationalgeographic.com.es/medio/2022/12/12/aguila-1_405f4994_221212153716_1280x720.jpg"
-                alt=""
-              />
-              <div>
-                <h3>Test Article Left</h3>
-                <br />
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Molestias corporis repellat quaerat incidunt sed ipsum
-                  recusandae blanditiis nobis ab iste, natus cum ipsa tenetur
-                  accusantium, minima eaque. Quo, delectus expedita.
-                </p>
-                <button>Leer Mas</button>
               </div>
             </article>
           </div>
